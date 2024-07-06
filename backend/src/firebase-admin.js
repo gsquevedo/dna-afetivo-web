bdcb220509fd8acca970f558a69aed05b867d05d
gsquevedo <gsquevedo@inf.ufsm.br>
2024-07-05 21:39:28 -0300

    feat: connection with firebase and login page and firebase credentials

--- backend/src/firebase-admin.js
+++ backend/src/firebase-admin.js
@@ -1,9 +1,21 @@
+require('dotenv').config();
 const admin = require('firebase-admin');
-const serviceAccount = require('../projeto-web-indigena-firebase-adminsdk-8namd-f7c26a4a3c.json');
+
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: "projeto-web-indigena.appspot.com"
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET
 });
 
 const db = admin.firestore();
